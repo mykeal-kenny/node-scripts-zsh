ab6c21abb5300ff19e1b919389f0e39557e344b4
fireinjun <mk@fireinjun.com>
2019-07-21 20:29:37 -0400

    added library files to repo

---
+++ node-scripts.zsh
@@ -0,0 +1,9 @@
+# check for Node installation
+# this is supposed to help node developers afterall
+
+# check for package.json file
+
+## if package.json is found
+# read package.json into node
+
+# check for node_modules folder 
